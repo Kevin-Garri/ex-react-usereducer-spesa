5255efa1cdde620d76c24fed2ddf481bff7b8c6b
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-06-06 17:01:52 +0200

    add: milestone2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 function App() {
 
   const products = [
@@ -7,6 +9,20 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([]);
+
+  const addToCart = (product) => {
+    const isProducAlreadyInCart = addedProducts.some(p => p.name === product.name);
+    if (isProducAlreadyInCart) {
+      return;
+    }
+    const productToAdd = {
+      ...product,
+      quantity: 1
+    } // Aggiungo la proprietà quantity con valore 1
+    setAddedProducts(curr => [...curr, productToAdd]);
+  }
+
   return (
     <>
       <h1>prodotti da scegliere</h1>
@@ -14,11 +30,24 @@ function App() {
         {products.map((p, index) => (
           <li key={index}>
             <p>{p.name} ({p.price.toFixed(2)}€)</p>
+            <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
             {/* toFixed(2) per mostrare il prezzo con due decimali */}
             {/* Utilizzo di index come chiave per ogni elemento della lista */}
           </li>
         ))}
       </ul>
+      {addedProducts.length > 0 && (
+        <>
+          <h2>Prodotti nel carrello</h2>
+          <ul>
+            {addedProducts.map((p, index) => (
+              <li key={index}>
+                <p>{p.quantity} x {p.name} ({p.price.toFixed(2)}€)</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   )
 }
@@ -34,3 +63,17 @@ Nome
 Prezzo
 
 Obiettivo: Vedere un elenco leggibile di tutti i prodotti con nome e prezzo.*/
+
+/*Milestone 2: Aggiungere prodotti al carrello
+Aggiungi uno stato locale addedProducts (inizialmente un array vuoto) per rappresentare i prodotti nel carrello.
+Per ogni prodotto della lista, aggiungi un bottone "Aggiungi al carrello":
+Al click del bottone, usa una funzione addToCart per:
+Aggiungere il prodotto al carrello se non è già presente, con una proprietà quantity = 1.
+Se il prodotto è già nel carrello, ignora l’azione.
+Sotto alla lista dei prodotti, mostra una lista dei prodotti nel carrello se addedProducts contiene almeno un elemento.
+Per ogni prodotto nel carrello, mostra:
+Nome
+Prezzo
+Quantità
+
+Obiettivo: L’utente può aggiungere prodotti al carrello e vedere una lista dei prodotti aggiunti.*/
